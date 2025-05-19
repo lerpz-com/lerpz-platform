78b5db7d02b9b4d1d7fc9b73b70de228f9ace061
Kasper Jønsson <dkkasjoe@hotmail.com>
2025-05-19 22:27:32 +0200

    Small docs formulation changes

--- lerpz-frontend/src/config.rs
+++ lerpz-frontend/src/config.rs
@@ -4,10 +4,9 @@ use std::sync::LazyLock;
 
 use lerpz_utils::{env::get_env, generate_config};
 
-/// The main configuration struct for the server.
+/// The main config for the server.
 /// 
-/// Lazy loaded using `LazyLock` to ensure that the configuration is only loaded
-/// once.
+/// Loaded using [`LazyLock`] to ensure that the config is only loaded once.
 pub static CONFIG: LazyLock<Config> = LazyLock::new(|| Config::from_env().unwrap());
 
 generate_config!(
