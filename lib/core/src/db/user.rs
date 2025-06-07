abe749c022d1ef6ebf86f95d7be631fa151081db
Kasper Jønsson <dkkasjoe@hotmail.com>
2025-06-07 18:03:37 +0200

    Started on token grant flows

--- lib/core/src/db/user.rs
+++ lib/core/src/db/user.rs
@@ -5,8 +5,9 @@ use uuid::Uuid;
 #[derive(Debug, Clone, FromRow)]
 pub struct User {
     pub id: Uuid,
-    pub name: String,
     pub email: String,
+    pub username: String,
+    pub password_hash: String,
     pub avatar: Option<String>,
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
--- svc/auth/src/api/oauth/token.rs
+++ svc/auth/src/api/oauth/token.rs
@@ -1,6 +1,41 @@
-use lerpz_utils::axum::error::HandlerResult;
+use axum::{Json, http::StatusCode};
+use lerpz_utils::axum::error::{HandlerError, HandlerResult};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[non_exhaustive]
+pub enum TokenGrantType {
+    AuthorizationCode(),
+    ClientCredentials(),
+    Password(PasswordGrant),
+    RefreshToken(String),
+    Unknown
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PasswordGrant {
+    pub username: String,
+    pub password: String,
+}
 
 #[axum::debug_handler]
-pub async fn handler() -> HandlerResult<()> {
+pub async fn handler(Json(body): Json<TokenGrantType>) -> HandlerResult<()> {
+    use TokenGrantType::*;
+
+    match body {
+        AuthorizationCode() => authorization_code_handler().await,
+        ClientCredentials() => Ok(()),
+        Password(_) => Ok(()),
+        RefreshToken(_) => Ok(()),
+        Unknown => Err(HandlerError::new(
+            StatusCode::BAD_REQUEST,
+            "Unsupported grant type.",
+            "The provided grant type is not supported.",
+        ))
+    }
+}
+
+pub async fn authorization_code_handler() -> HandlerResult<()> {
     Ok(())
 }
