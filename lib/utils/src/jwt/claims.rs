e6e9cac7caba198a9686322ee4c1dcc01423975b
Kasper Jønsson <dkkasjoe@hotmail.com>
2025-06-21 00:49:38 +0200

    Better claims

--- lib/utils/src/jwt/claims.rs
+++ lib/utils/src/jwt/claims.rs
@@ -1,21 +1,21 @@
 use serde::{Deserialize, Serialize};
-use uuid::Uuid;
 
 /// Represent all claims for a token.
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Claims {
+    /// What audience the token is for.
+    #[serde(skip_serializing_if = "String::is_empty")]
+    pub aud: String,
+    /// Who issued the token.
+    #[serde(skip_serializing_if = "String::is_empty")]
+    pub iss: String,
     /// Subject of the token.
-    pub sub: Uuid,
+    #[serde(skip_serializing_if = "String::is_empty")]
+    pub sub: String,
     /// Which time the token will expire.
     pub exp: i64,
     /// When the token will be valid.
     pub nbf: i64,
     /// When the token was issued.
     pub iat: i64,
-    /// Who issued the token.
-    #[serde(skip_serializing_if = "String::is_empty")]
-    pub iss: String,
-    /// What audience the token is for.
-    #[serde(skip_serializing_if = "String::is_empty")]
-    pub aud: String,
 }
