2bf0514acea16da902b185c819d3497a49b80cad
Kasper Jønsson <dkkasjoe@hotmail.com>
2025-07-14 20:46:12 +0200

    Added postgrestools LSP server

--- migrations/20250603115959_initial.sql
+++ migrations/20250603115959_initial.sql
@@ -1,3 +1,5 @@
+-- Initial SQL migration for Lerpz auth service
+
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE OR REPLACE FUNCTION update_timestamp()
@@ -8,7 +10,9 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TABLE IF NOT EXISTS organizations (
+-- Organizations
+
+CREATE TABLE IF NOT EXISTS organizations(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(64) NOT NULL UNIQUE,
     description TEXT DEFAULT NULL,
@@ -21,13 +25,15 @@ CREATE TRIGGER update_timestamp
     FOR EACH ROW
     EXECUTE FUNCTION update_timestamp();
 
-CREATE TABLE IF NOT EXISTS users (
+-- Users
+
+CREATE TABLE IF NOT EXISTS users(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     username VARCHAR(32) NOT NULL UNIQUE,
+    avatar VARCHAR(256) DEFAULT NULL,
     primary_email VARCHAR(64) NOT NULL UNIQUE,
     password_hash VARCHAR(128) NOT NULL,
     password_salt VARCHAR(64) NOT NULL,
-    avatar VARCHAR(256) DEFAULT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     organization_id UUID DEFAULT NULL REFERENCES organizations(id)
@@ -38,7 +44,26 @@ CREATE TRIGGER update_timestamp
     FOR EACH ROW
     EXECUTE FUNCTION update_timestamp();
 
-CREATE TABLE IF NOT EXISTS oauth_clients (
+-- User Consents
+
+CREATE TABLE IF NOT EXISTS user_client_authorizations(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID NOT NULL REFERENCES users(id),
+    client_id UUID NOT NULL REFERENCES oauth_clients(id),
+    scope TEXT NOT NULL,
+    authorized_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    expires_at TIMESTAMP DEFAULT NULL,
+    UNIQUE (user_id, client_id)
+);
+
+CREATE TRIGGER update_timestamp
+    BEFORE UPDATE ON user_client_authorizations
+    FOR EACH ROW
+    EXECUTE FUNCTION update_timestamp();
+
+-- OAuth Clients
+
+CREATE TABLE IF NOT EXISTS oauth_clients(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     secret VARCHAR(512) NOT NULL,
     name VARCHAR(64) NOT NULL UNIQUE,
@@ -53,7 +78,9 @@ CREATE TRIGGER update_timestamp
     FOR EACH ROW
     EXECUTE FUNCTION update_timestamp();
 
-CREATE TABLE IF NOT EXISTS redirect_uris (
+-- OAuth Redirect URIs
+
+CREATE TABLE IF NOT EXISTS redirect_uris(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     client_id UUID NOT NULL REFERENCES oauth_clients(id),
     uri VARCHAR(512) NOT NULL,
@@ -66,9 +93,79 @@ CREATE TRIGGER update_timestamp
     FOR EACH ROW
     EXECUTE FUNCTION update_timestamp();
 
-INSERT INTO oauth_clients (id, secret, name, description) VALUES (
+-- OAuth Scopes
+
+CREATE TABLE IF NOT EXISTS scopes(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(64) NOT NULL UNIQUE,
+    description TEXT,
+    parent_scope_id UUID DEFAULT NULL REFERENCES scopes(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TRIGGER update_timestamp
+    BEFORE UPDATE ON scopes
+    FOR EACH ROW
+    EXECUTE FUNCTION update_timestamp();
+
+CREATE TABLE IF NOT EXISTS client_scopes(
+    client_id UUID NOT NULL REFERENCES oauth_clients(id),
+    scope_id UUID NOT NULL REFERENCES scopes(id),
+    PRIMARY KEY (client_id, scope_id)
+);
+
+CREATE TRIGGER update_timestamp
+    BEFORE UPDATE ON client_scopes
+    FOR EACH ROW
+    EXECUTE FUNCTION update_timestamp();
+
+-- Access Tokens
+
+CREATE TABLE IF NOT EXISTS access_tokens(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    jti VARCHAR(128) NOT NULL UNIQUE,
+    user_id UUID DEFAULT NULL REFERENCES users(id),
+    client_id UUID NOT NULL REFERENCES oauth_clients(id),
+    scope TEXT,
+    expires_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    revoked_at TIMESTAMP DEFAULT NULL
+);
+
+CREATE TRIGGER update_timestamp
+    BEFORE UPDATE ON access_tokens
+    FOR EACH ROW
+    EXECUTE FUNCTION update_timestamp();
+
+-- Refresh Tokens
+
+CREATE TABLE IF NOT EXISTS refresh_tokens(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    token VARCHAR(512) NOT NULL UNIQUE,
+    user_id UUID NOT NULL REFERENCES users(id),
+    client_id UUID NOT NULL REFERENCES oauth_clients(id),
+    expires_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    revoked_at TIMESTAMP DEFAULT NULL
+);
+
+CREATE TRIGGER update_timestamp
+    BEFORE UPDATE ON refresh_tokens
+    FOR EACH ROW
+    EXECUTE FUNCTION update_timestamp();
+
+-- Seeding of Lerpz portal application
+
+INSERT INTO oauth_clients(
+    id,
+    secret,
+    name,
+    description
+) VALUES (
     'cdd37e5a-a554-4535-bff2-45ba130b05b4',
-    'secret-string',
+    'todo-add-secret-string',
     'Lerpz Portal',
     'The main client for the Lerpz portal.'
 )
