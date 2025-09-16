47fdca3fa6fb0bb812941b78b33b41f9a379f5d0
Kasper Jønsson <kas@lerpz.com>
2025-09-16 21:41:01 +0200

    Added database for auth

--- svc/portal/src/lib/auth.ts
+++ svc/portal/src/lib/auth.ts
@@ -1,7 +1,9 @@
 import { betterAuth } from "better-auth"
+import Database from "better-sqlite3"
 import { env } from "~/lib/env"
 
 export const auth = betterAuth({
+  database: new Database("./data/auth.db"),
   socialProviders: {
     microsoft: {
       clientId: env.ENTRA_ID_CLIENT_ID as string,
