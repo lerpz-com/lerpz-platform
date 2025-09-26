8bf846878213e86dadfd2f5cd427c436b2868a7f
Kasper Jønsson <daekjo@egmont.com>
2025-09-26 15:22:10 +0200

    Better 404 page

--- svc/portal/src/routes/[...404].tsx
+++ svc/portal/src/routes/[...404].tsx
@@ -1,10 +1,58 @@
+import { A } from "@solidjs/router"
 import { HttpStatusCode } from "@solidjs/start"
+import { Show } from "solid-js"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from "~/component/ui/card"
+
+interface Problem<D = unknown> {
+  type: string
+  title: string
+  detail: string
+  instance: string | null
+  extension: D
+  log_id: string | null
+}
 
 export default function NotFound() {
+  const problem: Problem = {
+    type: "/docs/errors/not-found",
+    title: "Not Found",
+    detail: "The requested resource was not found.",
+    instance: null,
+    extension: {},
+    log_id: null
+  }
+
   return (
     <>
       <HttpStatusCode code={404} />
-      <div>404 - Not Found</div>
+      <Card class="max-w-2xl">
+        <CardHeader>
+          <CardTitle class="text-center">404 - Not Found</CardTitle>
+          <CardDescription class="text-center">
+            The page you were looking for was not found.
+          </CardDescription>
+          <Show when={problem.type != "about:blank"}>
+            <A class="text-center" href={problem.type}>
+              Read more here.
+            </A>
+          </Show>
+        </CardHeader>
+        <CardContent>
+          <Card variant="outline">
+            <CardContent>
+              <pre class="whitespace-pre-wrap">
+                {JSON.stringify(problem, null, 2)}
+              </pre>
+            </CardContent>
+          </Card>
+        </CardContent>
+      </Card>
     </>
   )
 }
